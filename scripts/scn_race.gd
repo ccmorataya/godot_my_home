ab0d687c4f53f7faab13eef2bb7185b5c3db2f6f
Cristian Morataya <cris.morataya@gmail.com>
2019-01-27 17:32:25 -0600

    Improvements of scene 3

--- scripts/scn_race.gd
+++ scripts/scn_race.gd
@@ -9,6 +9,7 @@ onready var cs2d = CollisionShape2D.new()
 onready var rs2d = RectangleShape2D.new()
 var velocity = Vector2(0, 0)
 var colliding = false
+var sb2d_state = true
 
 func _ready():
 	collision_instance()
@@ -37,16 +38,33 @@ func _process(delta):
 		elif Input.is_action_pressed('ui_down'):
 			rigid_player.move_local_y(delta + 8)
 		bg.move_local_y(delta + 1)
-		sb2d.move_local_y(delta + 1)
+		if sb2d_state:
+			sb2d.move_local_y(delta + 1)
 		if bg.position.y >= 1024 && globals.player_life > 0:
 			print('you win')
 
 func _physics_process(delta):
 	var player_collision = rigid_player.move_and_collide(delta * velocity)
 	if player_collision:
+		print(player_collision.collider_id)
 		globals.player_life -= 10
-		rigid_player.set_position(Vector2(rigid_player.position.x, rigid_player.position.y)) # = Vector2(500, 480)
+		var x_pos = rigid_player.position.x
+		var y_pos = rigid_player.position.y
+		if player_collision.position.x < 130:
+			x_pos += 10
+		elif player_collision.position.x > 890:
+			x_pos -= 10
+		
+		if player_collision.position.y > 535:
+			y_pos -= 10
+		elif player_collision.position.y < 0:
+			y_pos += 15
+		
+		if player_collision.collider_id != 1138 && player_collision.collider_id != 1142 && player_collision.collider_id != 1140 && player_collision.collider_id != 1144:
+				player_collision.collider.free()
+		rigid_player.set_position(Vector2(x_pos, y_pos))
 		colliding = true
+		sb2d_state = false
 		message.show()
 
 func collision_instance():
@@ -55,3 +73,4 @@ func collision_instance():
 		sb2d.add_child(cs2d)
 		get_node(".").add_child(sb2d)
 		sb2d.position.x = 500
+		sb2d_state = true
